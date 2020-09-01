c6a280f4a67c3bb07e570befb78cb96385d6d1e5
njesquivel <njesquivel@gmail.com>
2020-08-31 23:03:34 -0600

    Starting GodClass

---
+++ GodClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Victor on 31/8/2020.
+//
+
+#include "GodClass.h"
---
+++ GodClass.h
@@ -0,0 +1,14 @@
+//
+// Created by Victor on 31/8/2020.
+//
+
+#ifndef BASIC_00_EXAMPLE_GODCLASS_H
+#define BASIC_00_EXAMPLE_GODCLASS_H
+
+
+class GodClass {
+
+};
+
+
+#endif //BASIC_00_EXAMPLE_GODCLASS_H
